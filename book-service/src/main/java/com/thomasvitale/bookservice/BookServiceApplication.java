2714aae508b72d1cdf79ac933de182cf742abd25
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-11-19 23:00:42 +0100

    OAuth2

--- book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/thomasvitale/bookservice/BookServiceApplication.java
@@ -2,7 +2,12 @@ package com.thomasvitale.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -11,10 +16,19 @@ import java.util.Map;
 import java.util.Optional;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class BookServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookServiceApplication.class, args);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .build();
     }
 
 }
--- edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/thomasvitale/edgeservice/EdgeServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,11 @@ public class EdgeServiceApplication {
                 .authorizeExchange(authorize -> authorize.anyExchange().authenticated())
                 .oauth2Login(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    ServerOAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository() {
+        return new WebSessionServerOAuth2AuthorizedClientRepository();
     }
 
 }
