e03315313e993e5bf32f049b27e8b3cbd2104202
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-11-20 08:28:41 +0100

    Add Angular SPA

---
+++ ui-application/src/app/fake-data.ts
@@ -0,0 +1,12 @@
+import { Book } from "./types";
+
+export const fakeBooks: Book[] = [{
+    isbn: '1234567890',
+    title: 'Harry Potter'
+}, {
+    isbn: '1234567891',
+    title: 'The Lord of the Rings'
+}, {
+    isbn: '1234567892',
+    title: 'His Dark Materials'
+}];
